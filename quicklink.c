69687cae7cb6b67b04833f56d224367317d29eaa
utkarshpatel7356 <134996234+utkarshpatel7356@users.noreply.github.com>
2023-11-04 17:46:31 +0530

    Update quicklink.c

--- quicklink.c
+++ quicklink.c
@@ -6,36 +6,24 @@
 
 #include<stdlib.h>
 #include<stdio.h>
-
 typedef struct AVLNode AVLNode;
 typedef struct BST BST;
 typedef struct Node Node;
 typedef struct EERTREE EERTREE;
-
 int max(int a, int b) {
-
     return a > b ? a : b;
 }
-
 struct AVLNode { 
-
 };
-
 AVLNode *createAVLNode(char key, Node *edge) {
-
 }
-
 int height(AVLNode *node) {
-
     return node ? node->height : -1;
 }
-
 int balance(AVLNode *node) {
 }
-
 void leftRotate(AVLNode **node) {
 }
-
 void rightRotate(AVLNode **node) {
 
     if(*node == NULL) return;
@@ -51,47 +39,32 @@ void rightRotate(AVLNode **node) {
     (*node)->right->height = max(height((*node)->right->left), height((*node)->right->right)) + 1;
     (*node)->height = max(height((*node)->left), height((*node)->right)) + 1;
 }
-
 Node *search(AVLNode *root, char key) {
 }
-
 void insert(AVLNode **root, AVLNode *node) {
 }
-
 struct BST {
 
     AVLNode *root;
 };
-
 BST *createBST() {
-
-
 }
-
 Node *searchBST(BST *bst, int key) {
-
 }
-
 void insertBST(BST *bst, int key, Node *edge) {
 
     insert(&bst->root, createAVLNode(key, edge));
 }
-
 struct Node { 
 };
-
 Node *createNode(int len) {
-
-
 }
-
 struct EERTREE { 
 
     Node *root_even, *root_odd; // Nodes representing empty string and imaginary string respectively
     Node *max_suf; // Node representing the longest suffix palindrome of the current string
     int size; // Number of non-trivial nodes in the EERTREE, i.e., number of palindromes in the current string
 };
-
 EERTREE *createEERTREE() {
 
     EERTREE *new_eertree = (EERTREE *) malloc(sizeof(EERTREE));
@@ -105,13 +78,9 @@ EERTREE *createEERTREE() {
 
     return new_eertree;
 }
-
 void add(EERTREE *eertree, char *str, int i) {
 }
-
 EERTREE *buildEERTREE(char *str) {
-
 }
-
 void main() {   
 }
